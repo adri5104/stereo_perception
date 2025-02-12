0580123e6ca0cec993baf10a725342d9a9206d3f
adrian rieker <adrian.rieker@tum.de>
2025-02-12 17:35:19 +0100

    created package for stereo computation to test with kitti dataset

---
+++ perception_pipeline/launch_pipeline/launch/launch_pipeline_kitti.launch.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+    
+    return launch.LaunchDescription([
+      
+        launch.actions.ExecuteProcess(
+          cmd=['ros2', 'bag', 'play', '-l', '/home/adrian/ros2_ws/src/stereo_perception/Datasets/rkitti_rosbag_0014.db3'],
+          output='screen'
+        ),
+
+        launch_ros.actions.Node(
+            package='optical_flow_computation', 
+            executable='optical_flow_computation',
+            remappings=[
+                ('left/image_raw', '/left/image_raw'),   
+                ('/optical_flow', 'perception_pipeline/optical_flow'),
+            ],
+            parameters=[{
+                'pyr_scale': 0.5  , 
+                'levels': 6,
+                'winsize': 15 , #35
+                'iterations': 10,
+                'poly_n': 5,
+                'poly_sigma': 1.5   ,
+                'flags': 0,
+            }],
+        ),
+        
+        launch_ros.actions.Node(
+            package='stereo_computation', 
+            executable='stereo_computation_node',
+            output='screen',
+            parameters=[
+              {"in_left_image_topic": "/left/image_raw"},
+              {"in_right_image_topic": "/right/image_raw"},
+              {"in_camera_info_topic": "/kitti/camera_gray_right/camera_info"},
+              {"out_depth_image_topic": "perception_pipeline/depth_image"},
+              {"out_disparity_image_topic": "perception_pipeline/disparity"},
+              ],
+        ),
+
+        launch_ros.actions.Node(
+            package='visual_odometry',
+            executable='visual_odometry_node',
+            parameters=[
+                {'color_image_topic': '/left/image_raw'},
+                {'depth_image_topic': 'perception_pipeline/depth_image'},
+                {'camera_info_topic': '/left/camera_info'},
+                {'odometry_debug_topic': '/debug/odometry_keypoints'},
+                {'camera_frame_tf_topic': '/perception_pipeline/camera_frame_tf'},
+                {'max_depth_odom' : 100000.0},
+                {'min_depth_odom' : 0.0},
+                {'odometry_debug_image' : True},
+              ],
+        ),       
+        
+        launch_ros.actions.Node(
+            package='kalman_filter',
+            executable='kalman_filter_node',
+            name='kalman_filter_node',
+            output='screen',
+            parameters=[
+                {'optical_flow_topic': '/perception_pipeline/optical_flow'},
+                {'depth_topic': "perception_pipeline/depth_image"},
+                {'camera_info_topic': '/kitti/camera_gray_right/camera_info'},
+                {'camera_frame_tf_topic': '/perception_pipeline/camera_frame_tf'},
+                {'color_image_topic':"/left/image_raw"}, 
+                {'debug_image_topic_':'/debug/debug_image'},
+                {'output_6d_topic' : '/perception_pipeline/output_6d'},
+                {'debug_markers_topic' : '/debug/image_6d_markers'},
+                {'grid_size' : 15},
+                {'use_ego_motion' : True},
+                {'sigma2_x_system' : 4.0},
+                {'sigma2_y_system' : 4.0},
+                {'sigma2_z_system' : 1.0},
+                {'sigma2_flow_y_measurement' : 2.0},
+                {'sigma2_flow_x_measurement' : 2.0},
+                {'sigma2_depth_system' : 1.0},
+                {'sigma2_tx_measurement' : 10.0},
+                {'sigma2_ty_measurement' : 10.0},
+                {'sigma2_tz_measurement' : 10.0},
+                {'sigma2_theta_measurement' : 10.0},
+                {'sigma2_psi_system' : 10.0},
+                {'min_depth' : 3.5},
+                {'max_depth' : 18.0},
+                {'min_height_' : -2.0},
+                {'max_height' : 4.0},
+            ],
+            ros_arguments= ["--log-level", "info"] 
+        ), 
+        
+        
+        launch_ros.actions.Node(
+            package='foxglove_bridge',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            output='screen',
+            parameters=[
+                {'send_buffer_limit:': 1000000000 },
+            ],
+        ),
+
+          
+        
+    ])  
---
+++ perception_pipeline/stereo_computation/nodes/stereo_computation_node.hpp
@@ -0,0 +1,86 @@
+#ifndef _STEREO_COMPUTATION_NODE_HPP_
+#define _STEREO_COMPUTATION_NODE_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+#ifdef ROS_VERSION_JAZZY
+#include <cv_bridge/cv_bridge.hpp>
+#else
+#include <cv_bridge/cv_bridge.h>
+#endif
+
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudastereo.hpp>
+
+namespace perception_pipeline
+{
+namespace stereo_computation 
+{
+
+using SyncPolicy = message_filters::sync_policies::ApproximateTime<
+    sensor_msgs::msg::Image,
+    sensor_msgs::msg::Image
+>;
+
+/**
+ * @class StereoComputationNode
+ * @brief ROS2 Node that subscribes to left and right images and camera info,
+ *       and computes the disparity map.
+ * 
+ */
+class StereoComputationNode : public rclcpp::Node
+{
+  public:
+    StereoComputationNode();
+
+  private:
+
+  // Callback for the message syncronizer
+  void updateSync(
+    const sensor_msgs::msg::Image::ConstSharedPtr left_image_msg,
+    const sensor_msgs::msg::Image::ConstSharedPtr right_image_msg);
+  // CameraInfo callback
+  void cameraInfoCallback(const sensor_msgs::msg::CameraInfo::ConstSharedPtr msg);
+
+  // Message filter subscribers
+  message_filters::Subscriber<sensor_msgs::msg::Image> left_image_sub_;
+  message_filters::Subscriber<sensor_msgs::msg::Image> right_image_sub_;
+
+  // Camera info ros subscriber without message filter
+  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_sub_;
+
+  // Synchronizer pointer
+  std::shared_ptr<message_filters::Synchronizer<SyncPolicy>> sync_;
+
+  // Publisher
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr depth_image_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr disparity_image_pub_;
+
+  // CUDA-based stereo matcher.
+  cv::Ptr<cv::cuda::StereoBM> stereoBM_; 
+
+  // Topic names
+  std::string in_left_image_topic_;
+  std::string in_right_image_topic_;
+  std::string in_camera_info_topic_;
+  std::string out_depth_image_topic_;
+  std::string out_disparity_image_topic_;
+
+  // Calibration parameters.
+  double focal_length_;  // typically from camera_info.k[0]
+  double baseline_;      
+  bool camera_parameters_set_ = false;
+
+};
+
+} // namespace stereo_computation
+} // namespace perception_pipeline
+
+
+#endif // _STEREO_COMPUTATION_NODE_HPP_
