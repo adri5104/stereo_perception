52727666bfac91e5bdd0ea1b36b8da39157ff683
adrian rieker <adrian.rieker@tum.de>
2025-05-18 18:54:30 +0200

    created docker image for bridge

---
+++ docker/stereo_perception_edgar_bridge/scripts/build_docker.sh
@@ -0,0 +1,14 @@
+# Default values
+#!/bin/bash
+PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../../.. &> /dev/null && pwd )"
+cd $PARENT_DIR
+
+GITNAME="gitlab.lrz.de:5005/teleoperiertes_fahren/research_brecht/stereo_perception"
+NAME="stereo_perception_edgar_bridge"
+TAG="1.0.0"
+FILE=${PARENT_DIR}/docker/$NAME/Dockerfile
+
+echo  $PARENT_DIR
+echo $FILE
+
+docker build -f $FILE --tag $GITNAME/$NAME:$TAG  $PARENT_DIR  
