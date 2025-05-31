6509dc1a35a60f1feaf495550e507e340ae7cf52
adrian rieker <adrian.rieker@tum.de>
2025-05-31 11:24:05 +0200

    fixed small errors

--- src/utils/stereo_perception_autoware_bridge/nodes/stereo_perception_autoware_bridge_node.cpp
+++ src/utils/stereo_perception_autoware_bridge/nodes/stereo_perception_autoware_bridge_node.cpp
@@ -15,9 +15,60 @@ namespace stereo_perception_autoware_bridge
 StereoPerceptionAutowareBridgeNode::StereoPerceptionAutowareBridgeNode()
 : Node("stereo_perception_autoware_bridge_node")
 {
-    RCLCPP_INFO(this->get_logger(), "Stereo Perception Autoware Bridge Node initialized");
+    // Declare parameters
+    declare_parameter<std::string>("clustered_object_array_topic", "/autoware/clustered_objects");
+    declare_parameter<std::string>("predicted_objects_topic", "/autoware/predicted_objects");
+
+    // Get parameters
+    get_parameter("clustered_object_array_topic", clustered_object_array_topic_);
+    get_parameter("predicted_objects_topic", predicted_objects_topic_);
+    RCLCPP_INFO(get_logger(), "Clustered Object Array Topic: %s", clustered_object_array_topic_.c_str());
+    RCLCPP_INFO(get_logger(), "Predicted Objects Topic: %s", predicted_objects_topic_.c_str());
+
+    // Initialize subscribers and publishers
+    clustered_object_array_pub_ = create_publisher<stereo_perception_msgs::msg::ClusteredObjectArray>
+      (
+        clustered_object_array_topic_, rclcpp::QoS(10)
+      );
     
+    predicted_objects_sub_ = create_subscription<autoware_perception_msgs::msg::PredictedObjects>
+    (
+      predicted_objects_topic_,
+      rclcpp::QoS(10),
+      std::bind(
+        &StereoPerceptionAutowareBridgeNode::autowarePredictedObjectsCallback, 
+        this, 
+        std::placeholders::_1
+      ) 
+    );
 }
+
+void StereoPerceptionAutowareBridgeNode::autowarePredictedObjectsCallback(
+    const autoware_perception_msgs::msg::PredictedObjects::SharedPtr msg)
+{
+    stereo_perception_msgs::msg::ClusteredObjectArray clustered_object_array;
+    clustered_object_array.header = msg->header;
+    clustered_object_array.num_clusters = msg->objects.size();
+
+    for (const auto & autoware_object : msg->objects) {
+        clustered_object_array.objects.push_back(convertPredictedObjectToClusteredObject(autoware_object));
+    }
+
+    clustered_object_array_pub_->publish(clustered_object_array);
+}
+
+stereo_perception_msgs::msg::ClusteredObject StereoPerceptionAutowareBridgeNode::convertPredictedObjectToClusteredObject(
+    const autoware_perception_msgs::msg::PredictedObject & autoware_object)
+{
+    stereo_perception_msgs::msg::ClusteredObject clustered_object;
+    clustered_object.position = autoware_object.kinematics.initial_pose_with_covariance.pose.position;
+    clustered_object.velocity = autoware_object.kinematics.initial_twist_with_covariance.twist.linear; 
+    return clustered_object;
+}
+
+
+
+
 
 } // namespace stereo_perception_autoware_bridge
 } // namespace utils
--- src/utils/stereo_perception_autoware_bridge/nodes/stereo_perception_autoware_bridge_node.hpp
+++ src/utils/stereo_perception_autoware_bridge/nodes/stereo_perception_autoware_bridge_node.hpp
@@ -11,8 +11,8 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/twist_with_covariance.hpp>
 
-#include "autoware_perception_msgs/msg/tracked_objects.hpp"
-#include "autoware_perception_msgs/msg/tracked_object_kinematics.hpp"
+#include "autoware_perception_msgs/msg/predicted_objects.hpp"
+#include "autoware_perception_msgs/msg/predicted_object_kinematics.hpp"
 #include "stereo_perception_msgs/msg/clustered_object_array.hpp"
 #include "stereo_perception_msgs/msg/clustered_object.hpp"
 #include "stereo_perception_msgs/msg/bounding_box.hpp"
@@ -26,7 +26,7 @@ namespace stereo_perception_autoware_bridge
 
 /**
  * @brief Node that bridges Autoware perception and stereo perception
- * This node subscribes to the Autoware tracked objects topic and converts it to a stereo perception clustered object array message.
+ * This node subscribes to the Autoware predicted objects topic and converts it to a stereo perception clustered object array message.
  * The published topics are in the camera_optical_frame, which is the same as the stereo camera frame.
  * Zero is the center of the camera, Z-axis is pointing forward, Y-axis is pointing to the left, and X-axis is pointing down.
  */
@@ -42,26 +42,26 @@ class StereoPerceptionAutowareBridgeNode : public rclcpp::Node
   private:
     
     /**
-     * @brief Callback for the Autoware tracked objects topic. Takes the received
-     * tracked objects message and converts it to a stereo perception clustered object array message.
+     * @brief Callback for the Autoware Predicted objects topic. Takes the received
+     * Predicted objects message and converts it to a stereo perception clustered object array message.
      * 
-     * @param msg The received Autoware tracked objects message
+     * @param msg The received Autoware Predicted objects message
      */
-    void autowareTrackedObjectsCallback(const autoware_perception_msgs::msg::TrackedObjects::SharedPtr msg);
+    void autowarePredictedObjectsCallback(const autoware_perception_msgs::msg::PredictedObjects::SharedPtr msg);
 
     /**
-     * @brief Converts a single Autoware tracked object to a stereo perception clustered object.
+     * @brief Converts a single Autoware Predicted object to a stereo perception clustered object.
      * 
-     * @param autoware_object The Autoware tracked object to convert
+     * @param autoware_object The Autoware Predicted object to convert
      * @return stereo_perception_msgs::msg::ClusteredObject The converted clustered object
      */
-    stereo_perception_msgs::msg::ClusteredObject convertTrackedObjectToClusteredObject(
-        const autoware_perception_msgs::msg::TrackedObject & autoware_object);
+    stereo_perception_msgs::msg::ClusteredObject convertPredictedObjectToClusteredObject(
+        const autoware_perception_msgs::msg::PredictedObject & autoware_object);
 
     rclcpp::Publisher<stereo_perception_msgs::msg::ClusteredObjectArray>::SharedPtr clustered_object_array_pub_; ///< Publisher for clustered object array messages.
-    rclcpp::Subscription<autoware_perception_msgs::msg::TrackedObjects>::SharedPtr tracked_objects_sub_; ///< Subscriber for Autoware tracked objects messages.
+    rclcpp::Subscription<autoware_perception_msgs::msg::PredictedObjects>::SharedPtr predicted_objects_sub_; ///< Subscriber for Autoware Predicted objects messages.
     std::string clustered_object_array_topic_ ;
-    std::string tracked_objects_topic_;
+    std::string predicted_objects_topic_;
     
 };
 
