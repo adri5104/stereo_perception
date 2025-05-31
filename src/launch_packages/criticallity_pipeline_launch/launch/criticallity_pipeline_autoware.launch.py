af7e1103513e7c5cb687c9a3235ff7beade78968
adrian rieker <adrian.rieker@tum.de>
2025-05-31 14:09:16 +0200

    fixed launch files of criticallity pipeline with autoware

---
+++ src/launch_packages/criticallity_pipeline_launch/launch/criticallity_pipeline_autoware.launch.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import launch 
+import launch_ros.actions
+
+def generate_launch_description():
+    use_sim_time = False
+    
+    return launch.LaunchDescription([    
+        launch_ros.actions.Node(
+            package='ttc_calculator',
+            executable='ttc_calculator_node',
+            name='ttc_calculator_node',
+            output='screen',
+            parameters=[
+                {'input_clusters_topic': '/perception_pipeline/autoware/output_clusters'},
+                {'input_twist_topic': '/perception_pipeline/ego_twist'},
+                {'input_path_topic': '/perception_pipeline/ego_path'},
+                {'output_ttc_topic': '/criticallity_pipeline/autoware/ttc'},
+                {'output_marker_topic': '/criticallity_pipeline/autoware/markers'},
+                {'ego_width': 1.0},
+                {'ego_length': 2.0},
+                {'ego_height': 1.0},
+                {'publish_ego_marker': True},
+                {'use_path': False},
+                {'use_sim_time': use_sim_time}
+            ],
+        )
+    ]) 
---
+++ src/launch_packages/criticallity_pipeline_launch/launch/criticallity_pipeline_autoware_container.launch.py
@@ -0,0 +1,20 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+
+    config_file_path = "/home/ubuntu/config/params.yaml"
+
+    return launch.LaunchDescription([
+        
+        launch_ros.actions.Node(
+            package='ttc_calculator',
+            executable='ttc_calculator_node',
+            name='ttc_calculator_node',
+            output='screen',
+            parameters=[config_file_path],
+        ),
+        
+    ])
