23c10517317af9ea897c9f604bf8f3a437559a52
adrian rieker <adrian.rieker@tum.de>
2025-06-02 13:13:28 +0200

    changed tag to the image that contains the tracing tools

--- docker/perception_pipeline/scripts/run_docker.sh
+++ docker/perception_pipeline/scripts/run_docker.sh
@@ -2,9 +2,7 @@
 PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../../.. &> /dev/null && pwd )"
 GITNAME="gitlab.lrz.de:5005/teleoperiertes_fahren/research_brecht/stereo_perception"
 NAME="perception_pipeline"
-TAG="1.0.0"
-
-
+TAG="1.0.1"
 
 docker run -it --rm \
     --name $NAME \
@@ -14,11 +12,15 @@ docker run -it --rm \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.Xauthority:/root/.Xauthority \
     -e DISPLAY=$DISPLAY \
+    --cap-add=SYS_PTRACE \
+    --cap-add=SYS_ADMIN \
+    --volume /sys/kernel/debug:/sys/kernel/debug \
     --gpus all \
     -v /dev/shm:/dev/shm \
     -v $PARENT_DIR/docker/$NAME/config/carnegie:/home/ubuntu/config \
     -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
     --ipc=host \
+    --pid=host \
     -e QT_X11_NO_MITSHM=1 \
     -e XAUTHORITY=/root/.Xauthority \
     $GITNAME/$NAME:$TAG \
