b58cec18e3995ee5d1c3a77075959b7992bce4d3
adrian rieker <adrian.rieker@tum.de>
2025-06-02 14:50:35 +0200

    updated tag

--- docker/perception_pipeline/scripts/build_docker.sh
+++ docker/perception_pipeline/scripts/build_docker.sh
@@ -5,7 +5,7 @@ cd $PARENT_DIR
 
 GITNAME="gitlab.lrz.de:5005/teleoperiertes_fahren/research_brecht/stereo_perception"
 NAME="perception_pipeline"
-TAG="1.0.0"
+TAG="1.0.1"
 FILE=${PARENT_DIR}/docker/$NAME/Dockerfile
 
 echo  $PARENT_DIR
